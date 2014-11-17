c37b4aa84b6d0d7d5784accfcddbfd74e1ea4804
NgaNguyenDuy <kyo1508@gmail.com>
2014-11-17 08:12:36 +0700

    update server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 var express = require('express'),
     path = require('path'),
+    http = require('http'),
+    multipart = require('connect-multiparty'),
+    m = multipart(),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     cookieSession = require('cookie-session'),
@@ -9,7 +12,8 @@ var express = require('express'),
     os = require('os'),
     app = express(),
     route = require('./route'),
-    config = require('./config');
+    config = require('./config'),
+    server, boot, shutdown;
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -19,19 +23,45 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
 app.use(bodyParser.json());
 
 app.use(favicon(path.join(__dirname, config.favicon)));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', route);
 
+app.get('/', route.index);
+app.post('/upload', m, route.upload.uploadHandler);
+
+
+// Handler any other route.
 app.use(function(req, res, next) {
-    res.type('text/plain');
+    res.type('text/html');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 
-module.exports = app;
+
+// Start server
+
+server = http.createServer(app);
+
+boot = function() {
+    server.listen(config.port, function() {
+        console.info('Server was running at port ' + config.port);
+    });
+};
+
+shutdown = function() {
+    server.close();
+};
+
+if(require.main === module) {
+    boot();
+} else {
+    console.info('Running app as module');
+    exports.boot = boot;
+    exports.shutdown = shutdown;
+}
